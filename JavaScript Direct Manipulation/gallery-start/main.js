3308c4f7736ba8a24d8999c0666f47c85fd94a26
Janavi Chadha <48561914+JanaviChadha@users.noreply.github.com>
2023-03-03 14:41:56 -0500

    upload gallery code

---
+++ JavaScript Direct Manipulation/gallery-start/main.js
@@ -0,0 +1,47 @@
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+
+const images = ['pic1.jpg', `pic2.jpg`, `pic3.jpg`, `pic4.jpg`, `pic5.jpg`];
+
+/* Declaring the alternative text for each image file */
+
+const alternateNames = {
+  'pic1.jpg' : 'img of eye zoomed in ',
+  'pic2.jpg' : 'wavy lins in rock',
+  'pic3.jpg' : 'white and purple flowers',
+  'pic4.jpg' : 'toumb drawings',
+  'pic5.jpg' : 'butterfly on a leaf '
+}
+
+
+/* Looping through images */
+
+for (const image of images) {
+    const newImage = document.createElement('img');
+    newImage.setAttribute('src', `images/${image}`);
+    newImage.setAttribute('alt', alternateNames[image]);
+    thumbBar.appendChild(newImage);
+    newImage.addEventListener('click', e => {
+      displayedImage.src = e.target.src;
+      displayedImage.alt = e.target.alt;
+    });
+  }
+/* Wiring up the Darken/Lighten button */
+
+btn.addEventListener('click', () => {
+    const btnClass = btn.getAttribute('class');
+    if (btnClass === 'dark') {
+      btn.setAttribute('class','light');
+      btn.textContent = 'Lighten';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+    } else {
+      btn.setAttribute('class','dark');
+      btn.textContent = 'Darken';
+      overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+    }
+  });
